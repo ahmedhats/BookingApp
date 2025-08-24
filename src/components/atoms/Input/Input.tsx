52bc96cef5df3e8568f3cfb00b82eb1153f938a3
Kix-85 <kareemaldeeb25@gmail.com>
2025-08-24 15:38:44 +0300

    input updates

---
+++ src/components/atoms/Input/Input.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+import colors from '../../../colors/colors';
+
+type InputProps ={
+    placeHolder: string;
+    autoCapitalize: 'none' | 'sentences' | 'words' | 'characters';
+    secureTextEntry?: boolean;
+}
+
+const styles = StyleSheet.create({
+  common:{
+    borderRadius:24,
+    borderColor: `${colors.borderColor}`,
+  }
+}) 
+
+const Input = (props: InputProps) => {
+  return (
+    <>
+      <TextInput placeholder={props.placeHolder} autoCorrect={false} secureTextEntry={props.secureTextEntry}  autoCapitalize={props.autoCapitalize} textContentType='password' />
+    </>
+  );
+};
+
+export default Input;
+
+
