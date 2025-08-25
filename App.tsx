bdf1c59ada289258be434d73bbebf79e8fa2afac
Kix-85 <kareemaldeeb25@gmail.com>
2025-08-25 16:27:20 +0300

    password finished

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import DropDown from "./src/components/atoms/AppDropDown";
-import Input from "./src/components/atoms/Input/Input";
+import Input from "./src/components/atoms/AppInput";
 
 export default function App() {
   return (
       <View style={styles.container}>
-        <Text>hello world </Text>
-        <StatusBar style="light" />
+        <Input placeHolder="Password" autoCapitalize="none" size="large" type="password"/>
+        <StatusBar style="auto" />
       </View>
   );
 }
