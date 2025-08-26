935c031011f8e4d4bad5ed980b21494713fa385b
ahmedhat <2023170001@cis.asu.edu.eg>
2025-08-26 12:21:16 +0300

    price atom modified, size prop is now a number , added perWhat prop also

--- src/components/atoms/Price/AppPrice.tsx
+++ src/components/atoms/Price/AppPrice.tsx
@@ -3,24 +3,33 @@ import { StyleProp, StyleSheet, Text, View } from "react-native";
 import colors from "../../../colors/colors";
 
 // example usage
-// <AppPrice content={22} size={{fontSize: '20px'}}/>
+// <AppPrice content={100} size={18} perWhat="night" />
 
 type AppPriceProps = {
     content: number,
-    size: StyleProp<any>
+    size: number,
+    perWhat: string
 }
 
-export default function AppPrice({ content, size }: AppPriceProps) {
+export default function AppPrice({ content, size, perWhat }: AppPriceProps) {
     return (
-        <View>
-            <Text style={[styles.text, size]}>{`$${content}`}</Text>
+        <View style={styles.container}>
+            <Text style={[styles.text, { fontSize: size }]}>{`$${content}`}</Text>
+            {perWhat && <Text style={[styles.perWhat,{ fontSize: size ? size / 1.5 : 12 } ]}>{`/${perWhat}`}</Text>}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center"
+    },
     text: {
         color: colors.primary,
         fontWeight: "bold"
+    },
+    perWhat: {
+        color: "white",
     }
 })
