1e70a4639b0ea2dd77f80a3bc4ad66610d01d2a9
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-08-26 12:29:52 +0300

    Add TripCategoryTab molecule

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import DropDown from "./src/components/atoms/AppDropDown";
 import Input from "./src/components/atoms/Input/Input";
+import TripCategoryTab from "./src/components/molecules/TripCategoryTab";
 
 export default function App() {
   return (
       <View style={styles.container}>
         <Text>hello world </Text>
+        <TripCategoryTab imageUrl={"https://up.yimg.com/ib/th/id/OIP.CjuJhR9wS4jD9R183-cdDwHaEK?pid=Api&rs=1&c=1&qlt=95&w=198&h=111"} title={"Example Title"} />
         <StatusBar style="light" />
       </View>
   );
